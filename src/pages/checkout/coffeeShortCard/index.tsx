11a560d3bf53e8a383d84612db01fb96d3974f8c
Luiz Henrique <luizsacramento.tds@gmail.com>
2023-07-18 10:52:14 -0300

    and again

--- src/pages/checkout/coffeeShortCard/index.tsx
+++ src/pages/checkout/coffeeShortCard/index.tsx
@@ -33,6 +33,19 @@ export function ShortCardCoffee({name}: ShortCardCoffeeProps){
                                         name: iten.name,
                                         amount: 1,
                                         price: iten.price,
+                                        delivery :{
+                                            address: {
+                                                cep:  '',
+                                                rua: '',
+                                                numero:  '',
+                                                bairro:  '',
+                                                cidade:  '',
+                                                complemento:  '',
+                                                uf:  '',
+                                            },
+                                        phone:  '',
+                                        methodPayment: '',
+                                        } 
                                     })
                                 }
                             }}><Plus size={14}/></button>
@@ -43,6 +56,19 @@ export function ShortCardCoffee({name}: ShortCardCoffeeProps){
                                         name: iten.name,
                                         amount: -1,
                                         price: iten.price,
+                                        delivery :{
+                                            address: {
+                                                cep:  '',
+                                                rua: '',
+                                                numero:  '',
+                                                bairro:  '',
+                                                cidade:  '',
+                                                complemento:  '',
+                                                uf:  '',
+                                            },
+                                        phone:  '',
+                                        methodPayment: '',
+                                        } 
                                     })
                                 }
                             }}><Minus size={14}/></button>
